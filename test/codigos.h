ebc34ba3e355a2c0c53af5e2be23bc0f2d31da5c
josediegosantillan <santillanjosediego@gmail.com>
2025-12-05 15:40:43 -0300

    Prueba

---
+++ test/codigos.h
@@ -0,0 +1,8 @@
+// define que code: 233948488032
+#ifndef CODIGOS_H
+#define CODIGOS_H
+const int CODIGO_EXITO = 0;
+const int CODIGO_ERROR_CONEXION = 1;
+const int CODIGO_ERROR_SENSOR = 2;
+const int CODIGO_ERROR_RELE = 3;
+#endif // CODIGOS_H
